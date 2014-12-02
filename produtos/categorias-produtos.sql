42dc6cf09565bea4891b6282f1e2ce0d0d442929
Jonatan <web-jonatan@live.com>
2014-12-02 15:01:16 -0200

    Update and rename produtos.sql to produtos/categorias-produtos.sql

--- produtos/categorias-produtos.sql
+++ produtos/categorias-produtos.sql
@@ -48,6 +48,23 @@ catalog_product_super_attribute_label
 catalog_product_super_attribute_pricing
 catalog_product_super_link
 catalog_product_website
+catalog_category_anc_categs_index_idx
+catalog_category_anc_categs_index_tmp
+catalog_category_anc_products_index_idx
+catalog_category_anc_products_index_tmp
+catalog_category_entity
+catalog_category_entity_datetime
+catalog_category_entity_decimal
+catalog_category_entity_int
+catalog_category_entity_text
+catalog_category_entity_varchar
+catalog_category_flat_store_1
+catalog_category_product
+catalog_category_product_index
+catalog_category_product_index_enbl_idx
+catalog_category_product_index_enbl_tmp
+catalog_category_product_index_idx
+catalog_category_product_index_tmp
 
 ========================
 Deletar
